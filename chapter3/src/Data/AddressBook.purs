af609222173140f758ab9a17e3d025161c8a0166
Mukesh Soni <mukeshsoni@gmail.com>
2018-02-26 10:35:54 +0530

    chapter 3 - added the stuff from the book chapter

--- chapter3/src/Data/AddressBook.purs
+++ chapter3/src/Data/AddressBook.purs
@@ -3,3 +3,33 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
+import Data.List (List(..), filter, head)
+import Data.Maybe (Maybe)
+
+type Entry = 
+    { firstName :: String
+    , lastName :: String
+    , address :: Address
+    }
+
+type Address = 
+    { street :: String
+    , city :: String
+    , state :: String
+    }
+
+type AddressBook = List Entry
+
+showEntry :: Entry -> String
+showEntry entry = entry.firstName <> ", " <>
+                  entry.lastName <> ": " <>
+                  showAddress entry.address
+
+showAddress :: Address -> String
+showAddress address = address.street <> ", " <> address.city <> ", " <> address.state
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+emptyBook :: AddressBook
+emptyBook = empty
