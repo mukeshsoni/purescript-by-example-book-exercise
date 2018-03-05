174685171c8fd0dc3369f3a9a4e138c729e6a420
Mukesh Soni <mukeshsoni@gmail.com>
2018-03-05 19:22:21 +0530

    chapter 6 - added solutions to  3 exercises

--- chapter6/src/Exercises.purs
+++ chapter6/src/Exercises.purs
@@ -13,3 +13,20 @@ instance showComplex :: Show Complex where
 instance eqComplex :: Eq Complex where
     eq (Complex c1) (Complex c2) = c1.real == c2.real && c1.imaginary == c2.imaginary
 
+data NonEmpty a = NonEmpty a (Array a)
+
+instance showNonEmpty :: (Show a) => Show (NonEmpty a) where
+    show (NonEmpty a arr) = "NonEmpty " <> show a <> " " <> show arr
+
+-- 1. Write an Eq instance for the type `NonEmpty a` which reuses the instances for `Eq a` and Eq (Array a)
+instance eqNonEmpty :: (Eq a, Eq (Array a)) => Eq (NonEmpty a) where
+    eq (NonEmpty a1 arr1) (NonEmpty a2 arr2) = a1 == a2 && arr1 == arr2
+
+-- 2. Write a Semigroup instance for `NonEmpty a` by reusing the Semigroup instance for `Array`
+instance appendNonEmpty :: (Semigroup (Array a)) => Semigroup (NonEmpty a) where
+    append (NonEmpty a1 arr1) (NonEmpty a2 arr2) = NonEmpty a1 (arr1 <> [a2] <> arr2)
+
+
+-- 3. Write a functor instance for `NonEmpty`
+instance mapNonEmpty :: Functor NonEmpty where
+    map f (NonEmpty a arr) = NonEmpty (f a) (map f arr)
