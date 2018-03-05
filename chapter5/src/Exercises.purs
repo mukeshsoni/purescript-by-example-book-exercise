e51cb3c5dd2e11915d71c253cf66e99beff7fc8e
Mukesh Soni <mukeshsoni@gmail.com>
2018-03-05 10:21:17 +0530

    chapter 5 - wrote functions for factorial and binomial coefficients using pattern matching

--- chapter5/src/Exercises.purs
+++ chapter5/src/Exercises.purs
@@ -5,6 +5,15 @@ import Prelude
 type Address = { street :: String, city :: String }
 type Person = { name :: String, address :: Address }
 
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+binomialCoefficents :: Int -> Int -> Int
+binomialCoefficents n k | k > n = 0
+binomialCoefficents _ 0 = 1
+binomialCoefficents n r = binomialCoefficents (n - 1) r + binomialCoefficents (n - 1) (r - 1)
+
 sameCity :: Person -> Person -> Boolean
 sameCity { address : {city : c1 }} { address : {city : c2 }} = c1 == c2
 
