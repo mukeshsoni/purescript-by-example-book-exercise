a671f3c6a877005cd74152f5d45250198ecbfe6a
Mukesh Soni <mukeshsoni@gmail.com>
2018-03-16 17:23:27 +0530

    chapter 7 - validate state to check for atleast 3 alpha numeric characters

--- chapter7/src/Data/AddressBook/Validation.purs
+++ chapter7/src/Data/AddressBook/Validation.purs
@@ -30,3 +30,29 @@ phoneNumberRegex =
     case regex "^\\d{3}-\\d{3}-\\d{4}$" noFlags of
       Right r -> r
 
+validateAddress :: Address -> V Errors Address
+validateAddress (Address o) =
+  address <$> (nonEmpty "Street" o.street *> pure o.street)
+          <*> (nonEmpty "City" o.city *> pure o.city)
+          <*> (lengthIs "State" 2 o.state *> pure o.state)
+
+matches :: String -> Regex -> String -> V Errors Unit
+matches _ regex value | test regex value = pure unit
+matches field _ _ = invalid ["Field '" <> field <> "' does not match the required format"]
+
+validatePhoneNumber :: PhoneNumber -> V Errors PhoneNumber
+validatePhoneNumber (PhoneNumber o) =
+  phoneNumber <$> pure o."type"
+              <*> (matches "Number" phoneNumberRegex o.number *> pure o.number)
+
+stateRegex :: Regex 
+stateRegex =
+  unsafePartial
+    case regex ".*[a-zA-Z0-9]{3}.*" noFlags of
+      Right r -> r
+
+validateAddress2 :: Address -> V Errors Address
+validateAddress2 (Address o) =
+  address <$> (nonEmpty "Street" o.street *> pure o.street)
+          <*> (nonEmpty "City" o.city *> pure o.city)
+          <*> (matches "State" stateRegex o.state *> pure o.state)
